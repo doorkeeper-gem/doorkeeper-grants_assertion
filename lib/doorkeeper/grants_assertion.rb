1eea439841f21c32b9ab8eb4a579f20f62e3cd20
Christopher Hein <me@christopherhein.com>
2014-08-06 08:55:48 -0700

    Fixing up houndci

--- lib/doorkeeper/grants_assertion.rb
+++ lib/doorkeeper/grants_assertion.rb
@@ -1,8 +1,8 @@
-require 'doorkeeper/request/assertion'
-require 'doorkeeper/grants_assertion/railtie'
+require "doorkeeper/request/assertion"
+require "doorkeeper/grants_assertion/railtie"
 
 module Doorkeeper
-  module GrantsAssertion    
+  module GrantsAssertion
     def resource_owner_from_assertion
       instance_eval(&Doorkeeper.configuration.resource_owner_from_assertion)
     end
@@ -20,7 +20,7 @@ end
 module Doorkeeper
   class Config
     option :resource_owner_from_assertion, default: (lambda do |routes|
-        warn(I18n.translate('doorkeeper.errors.messages.assertion_flow_not_configured'))
+        warn(I18n.translate("doorkeeper.errors.messages.assertion_flow_not_configured"))
         nil
       end)
   end
--- lib/doorkeeper/grants_assertion/railtie.rb
+++ lib/doorkeeper/grants_assertion/railtie.rb
@@ -1,11 +1,11 @@
 module Doorkeeper
   module GrantsAssertion
     class Railtie < ::Rails::Railtie
-      
+
       initializer "doorkeeper.grants_assertion" do
         Doorkeeper::ApplicationController.send :include, Doorkeeper::GrantsAssertion
       end
-      
+
     end
   end
 end
