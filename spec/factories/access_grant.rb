18a385c9d4224ee4eafb94b54c691fd39ac9adba
Pedro Carmona <pcarmona1990@gmail.com>
2018-03-27 01:06:59 +0200

    fix typo

--- spec/factories/access_grant.rb
+++ spec/factories/access_grant.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true.
+# frozen_string_literal: true
 
 FactoryBot.define do
   factory :access_grant, class: Doorkeeper::AccessGrant do
--- spec/support/dependencies/factory_bot.rb
+++ spec/support/dependencies/factory_bot.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true.
+# frozen_string_literal: true
 
 require 'factory_bot'
 FactoryBot.find_definitions
