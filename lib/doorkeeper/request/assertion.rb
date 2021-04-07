dbfd1f226e91203f20f253bc32e6ee8ab8c56971
Nikita Bulai <bulaj.nikita@gmail.com>
2021-04-07 17:54:49 +0300

    Fix PasswordAccessTokenRequest

--- lib/doorkeeper/request/assertion.rb
+++ lib/doorkeeper/request/assertion.rb
@@ -6,12 +6,7 @@ module Doorkeeper
       delegate :credentials, :resource_owner_from_assertion, :parameters, to: :server
 
       def request
-        @request ||= OAuth::PasswordAccessTokenRequest.new(
-          Doorkeeper.configuration,
-          client,
-          resource_owner_from_assertion,
-          parameters
-        )
+        @request ||= build_request
       end
 
       def authorize
@@ -19,6 +14,25 @@ module Doorkeeper
       end
 
       private
+
+      def build_request
+        if ::Gem::Dependency.new('', '>= 5.5.1').match?('', ::Doorkeeper::VERSION::STRING)
+          OAuth::PasswordAccessTokenRequest.new(
+            Doorkeeper.configuration,
+            client,
+            credentials,
+            resource_owner_from_assertion,
+            parameters
+          )
+        else
+          OAuth::PasswordAccessTokenRequest.new(
+            Doorkeeper.configuration,
+            client,
+            resource_owner_from_assertion,
+            parameters
+          )
+        end
+      end
 
       def client
         if credentials
